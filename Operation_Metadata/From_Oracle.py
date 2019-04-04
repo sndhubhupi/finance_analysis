c4622ec6d79cb7a1e7c1c5770a91ec18c29202cc
sndhubhupi <48937156+sndhubhupi@users.noreply.github.com>
2019-04-04 13:01:42 +0530

    Update From_Oracle.py
    fetch_stock_list added, removed fetch_stock_dt_range

--- Operation_Metadata/From_Oracle.py
+++ Operation_Metadata/From_Oracle.py
@@ -6,16 +6,16 @@ import proj_constant_var as const
 default_files_dir = os.getcwd()+ const.folder_to_process_file
 conn_str = cx_Oracle.connect(const.database_connection)
 
-def fetch_stock_list_dt_range():
+def fetch_stock_list():
     print '-- Fetching stock list with date range --'
     from_oracle_cursr = conn_str.cursor();
-    from_oracle_cursr.execute("select * from table(finance_analysis.out_stock_list_dt_range())");
-    stock_dt_range = from_oracle_cursr.fetchall();
+    from_oracle_cursr.execute("select * from table(finance_analysis.out_stock_list())");
+    stock_list = from_oracle_cursr.fetchall();
     from_oracle_cursr.close()
     #conn_str.commit()
     #conn_str.close()
     print '-- Fetching completed --'
-    return stock_dt_range
+    return stock_list
 
 def fetch_candlestick_findings():
     print '-- fetch_candlestick_findings --'
