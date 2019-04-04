47d2126cc69ea1073e011137837ecc21b5e5e43c
sndhubhupi <48937156+sndhubhupi@users.noreply.github.com>
2019-04-04 13:08:19 +0530

    Update daily_hist_load.py
    updated flow to get stock list from oracle on latest date check

--- Operation_Metadata/daily_hist_load.py
+++ Operation_Metadata/daily_hist_load.py
@@ -5,20 +5,20 @@ import Get_Prices
 import pandas as pd
 
 def load_daily_price_data() :
-    #To_Oracle.insert_data_to_stock_list()
-    #Get_Prices.load_stock_list()
-    #Get_Prices.run_load_for_stock_list()
-    #To_Oracle.load_all_download_price_to_db()
-    #To_Oracle.update_earliest_latest_dt()
-    #To_Oracle.calc_moving_average()
-    #To_Oracle.find_candle_stick_pattern()
+    To_Oracle.insert_data_to_stock_list()
+    stock_list = From_Oracle.fetch_stock_list()
+    Get_Prices.run_load_for_stock_list(stock_list)
+    To_Oracle.load_all_download_price_to_db()
+    To_Oracle.update_earliest_latest_dt()
+    To_Oracle.calc_moving_average()
+    To_Oracle.find_candle_stick_pattern()
     findings = From_Oracle.fetch_candlestick_findings()
     for x in findings:
         To_Telegram.sendTelegram(x,464308445)
         To_Telegram.sendTelegram(x, 506426930)
     labels = ['Stock Ticker', 'Date', 'Finding_Type', 'Discription']
     df = pd.DataFrame.from_records(findings, columns=labels)
-    df.to_csv('Findings_20190403.csv')
+    df.to_csv('Findings.csv')
 
 
 load_daily_price_data()
