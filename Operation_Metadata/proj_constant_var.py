d95435b4dbafeab58215dd2b755bec33c9efc0c4
sndhubhupi <48937156+sndhubhupi@users.noreply.github.com>
2019-04-04 16:53:01 +0530

    Update proj_constant_var.py

--- Operation_Metadata/proj_constant_var.py
+++ Operation_Metadata/proj_constant_var.py
@@ -3,5 +3,6 @@ downloaded_csv_folder = '/Download_csv/';
 database_connection = 'db_hist_data/db_hist_data@localhost/orcl' ;
 csv_extention = '.csv'
 stock_list_file = 'stock_list.csv' ;
+telegram_id_list = ['464308445','506426930']
 api_keys = ['TJFXVG58IVH4N7OM','8EL6PXCIYJD56OE8','2DEF4M6SGR4S5H0B','6VWDXQX7F23UYOV8','A5R41DURGC8MQJHM',
             'XE3BFLI3SBKRONE4','VNHUMPT6WPDFSHS2','QFQITXIAWTZBDUAM','20UMVZQY1T4PPB8Q','K28LMSFMYGL45IL2'];
