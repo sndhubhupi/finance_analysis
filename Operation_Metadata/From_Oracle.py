cfa9b0946556c9f42c91bdbcc1f2892eaff68386
sndhubhupi <48937156+sndhubhupi@users.noreply.github.com>
2019-04-05 11:02:14 +0530

    Update From_Oracle.py

--- Operation_Metadata/From_Oracle.py
+++ Operation_Metadata/From_Oracle.py
@@ -7,23 +7,23 @@ default_files_dir = os.getcwd()+ const.folder_to_process_file
 conn_str = cx_Oracle.connect(const.database_connection)
 
 def fetch_stock_list():
-    print '-- Fetching stock list to load data --'
+    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '-- Fetching stock list to load data --'
     from_oracle_cursr = conn_str.cursor();
     from_oracle_cursr.execute("select * from table(finance_analysis.out_stock_list())");
     stock_list = from_oracle_cursr.fetchall();
     from_oracle_cursr.close()
     #conn_str.commit()
     #conn_str.close()
-    print '-- Fetching completed --'
+    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '-- Fetching completed --'
     return stock_list
 
 def fetch_candlestick_findings():
-    print '-- fetch_candlestick_findings --'
+    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '-- fetch_candlestick_findings --'
     from_oracle_cursr = conn_str.cursor();
     from_oracle_cursr.execute("select * from table(finance_analysis.out_candle_stick_pattern())");
     findings = from_oracle_cursr.fetchall();
     from_oracle_cursr.close()
     #conn_str.commit()
     #conn_str.close()
-    print '-- fetch_candlestick_findings completed --'
+    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '-- fetch_candlestick_findings completed --'
     return findings
