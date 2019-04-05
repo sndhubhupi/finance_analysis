3e785fd72c6f1d311573682db77d2539e3a0e03a
sndhubhupi <48937156+sndhubhupi@users.noreply.github.com>
2019-04-05 11:13:24 +0530

    Update From_Oracle.py

--- Operation_Metadata/From_Oracle.py
+++ Operation_Metadata/From_Oracle.py
@@ -2,6 +2,7 @@ import cx_Oracle
 import csv
 import os as os
 import proj_constant_var as const
+import datetime
 
 default_files_dir = os.getcwd()+ const.folder_to_process_file
 conn_str = cx_Oracle.connect(const.database_connection)
