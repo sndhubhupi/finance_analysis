80b2bca7874b96ddf5b038772f2b5b627b6cfbae
sndhubhupi <48937156+sndhubhupi@users.noreply.github.com>
2019-04-05 11:12:54 +0530

    Update To_Telegram.py

--- Operation_Metadata/To_Telegram.py
+++ Operation_Metadata/To_Telegram.py
@@ -1,11 +1,11 @@
 import telegram
 import requests
 import time
-
+import datetime
 
 def sendTelegram(totalResponse, chatId):
     try:
-        print("in sendTelegram", totalResponse)
+        #print("in sendTelegram", totalResponse)
         bot_id = "bot564398612:AAEXUIfrJVFHfBnxS4Uot0Ob5vDPN8Ws69I"
         url = "https://api.telegram.org/" + bot_id + "/sendMessage?chat_id=" + str(chatId) + "&text= " + str(totalResponse)
         requests.get(url)
@@ -15,3 +15,9 @@ def sendTelegram(totalResponse, chatId):
         time.sleep(30)
         sendTelegram(totalResponse, chatId)
 
+def send_text_to_telegram(findings):
+    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') +" : Sending Text to telegram Started"
+    for id in const.telegram_id_list:
+        for record in findings:
+            sendTelegram(record,id)
+    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') +" : Sending Text to telegram Finished"
